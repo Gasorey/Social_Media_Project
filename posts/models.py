e95476f78222c678fd87dafa9022a6ca44bea01f
Gabriel <gasorey@gmail.com>
2020-03-25 17:01:17 -0300

    Debugging

--- posts/models.py
+++ posts/models.py
@@ -11,7 +11,7 @@ User = get_user_model()
 
 class Post(models.Model):
   user = models.ForeignKey(User,related_name='posts',on_delete=models.CASCADE)
-  created_at = models.DateTimeField(auto_now=True,on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now=True)
   message = models.TextField()
   message_html = models.TextField(editable=False)
   group = models.ForeignKey(Group,related_name='posts',null=True,blank=True,on_delete=models.CASCADE)
@@ -24,7 +24,7 @@ class Post(models.Model):
     super().save(*args,**kwargs)
 
   def get_absolute_url(self):
-    return reverse('posts:single',kwargs={'username':self.user.username,'pk':self.pk})
+    return reverse('posts:single',kwargs={'username': self.user.username,'pk':self.pk})
 
   class Meta:
     ordering = ['-created_at']
